12ea1e35a2e332398fdef6a4ed7ccda38c462292
PapaOso <escalantejose22@gmail.com>
2022-05-07 18:59:15 -0600

    Me voy a Dormir

--- Scrpts/cap2.js
+++ Scrpts/cap2.js
@@ -1,3 +1,17 @@
- let Frutas=["manzana","Uva","Pera"];
 
-document.write(Frutas[0])
+let pc ={
+    name:'Red_Douten',
+    procesador:'Intel i5',
+    ram:'8gb',
+    espacio:'750gb'
+};
+let nombre=pc["name"];
+frase= `El nombre de mi pc es ${pc["name"]} <br>
+        El procesador es ${pc["procesador"]} <br>
+        La Ram es de <b> ${pc["ram"]}</b> <br>
+        El espacio es de ${pc["espacio"]} <br>
+` 
+
+
+
+document.write(frase)
