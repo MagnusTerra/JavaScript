1fda3e391c30331fbf8f00aff950772839346b1b
PapaOso <escalantejose22@gmail.com>
2022-05-09 16:37:44 -0600

    POO2

---
+++ Cap3/POO.js
@@ -0,0 +1,15 @@
+class animal{
+    constructor(especie,edad,color){
+        this.especie = especie;
+        this.edad = edad;
+        this.color = color;
+    }
+    informacion(){
+        return `Soy un ${this.especie} y tengo ${this.edad} años
+        y soy de color ${this.color}`
+    }
+}
+let perro=new animal("Perro",5,"Rojo");
+
+document.getElementByI("Parra").perro.informacion()
+
