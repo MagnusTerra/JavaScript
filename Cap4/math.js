251f6966f5166538c5403e9a27f903e768d1299d
PapaOso <escalantejose22@gmail.com>
2022-05-12 20:52:07 -0600

    Final del cap4

--- Cap4/math.js
+++ Cap4/math.js
@@ -1,1 +1,17 @@
 document.write("<h1>Matematicas</h1>")
+numero=Math.sqrt(25)//Raiz cuadrada
+document.write(numero+"<br>")
+numero=Math.cbrt(27)//Raiz Cubica
+document.write(numero+"<br>")
+numero=Math.max(1,2,3,4,5,6,7,8,9)//devuelve el numero mas alto
+document.write(numero+"<br>")
+numero=Math.min(1,2,3,4,5,6,7,)
+document.write(numero+"<br>")
+numero=Math.random()*100
+numero=Math.round(numero)
+document.write(numero+"<br>")
+numero=Math.trunc(9.6)//quita los decimales
+document.write(numero+"<br>")
+numero=Math.PI;
+document.write(numero+"<br>")
+
