a96a3f185f5e56be080629c58bd77e5214bb8b22
PapaOso <escalantejose22@gmail.com>
2022-05-13 16:12:49 -0600

    Cambios de Cap4

--- Cap5/cap5.js
+++ Cap5/cap5.js
@@ -1,1 +1,17 @@
 document.write("<H1>Capiputo 5</h1>")
+//console.clear()//limpia la consola 
+console.error("Loco que haces")//para Generar un error en consola
+console.info("hola como Info")//Genera un mensaje de tipo informacion   
+console.log("Hola como mensaje normal")//Genera un mensaje normal   
+console.table([1,2,3,4,5,6])
+console.warn("tranquilo loco")//genera una avertencia 
+console.count()//Genera un contador
+console.count()
+console.count()
+console.count()
+console.count()
+console.countReset()//Resetea el contador
+console.count()
+console.time()//Empieza un contador de tiempo
+console.timeLog()//muestra el contador de tiempo
+console.timeEnd()//Termina el contador de tiempo
